d977f15c3b1e770b807c64145d7e233bb0b25aa3
ines uwase <uwaseines10@gmail.com>
2019-09-05 15:26:30 +0200

    created a readme

--- password.py
+++ password.py
@@ -1,16 +1,16 @@
-
 class User:
     """
-    Class that generates new instances of contacts.
+    Class that generates new instances of user.
     """
+    contact_list = [] # Empty contact list
 
-    contact_list = [] 
-
-    def __init__(self,user_name,number,email,password):
+    def __init__(self,first_name,last_name,number,email,user_name,password):
 
       # docstring removed for simplicity
 
-        self.user_name = user_name
+        self.first_name = first_name
+        self.last_name = last_name
         self.phone_number = number
         self.email = email
+        self.user_name = user_name
         self.password = password
